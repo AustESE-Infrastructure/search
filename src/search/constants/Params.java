59b5d1c1eaf1eaa7c2c9b62bb383405c97390da9
schmidda <desmond.allan.schmidt@gmail.com>
2015-01-24 15:18:19 +1000

    Added Find for real now.

---
+++ src/search/constants/Params.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search.constants;
+
+/**
+ *
+ * @author desmond
+ */
+public class Params {
+    public static String QUERY = "query";
+    public static String LANGUAGE = "language";
+    public static String START = "start";
+}
